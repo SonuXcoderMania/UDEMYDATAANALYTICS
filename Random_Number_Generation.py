a7535e3ad2af44c65158242904627f5eec7bcb8a
SonuXcoderMania <sonukumaryadav590598@gmail.com>
2025-02-17 11:19:14 +0530

    git commit -m

---
+++ Random_Number_Generation.py
@@ -0,0 +1,11 @@
+import numpy as np
+"""random_floats=np.random.rand(5)
+print(random_floats)"""
+
+#random_integer=np.random.randint(10,50, size=4)
+#print("random integers:",random_integer)
+
+normal_distribution=np.random.randn(6)
+print("normal distributions:",normal_distribution)
+
+
--- Working with Multidimensional numpy.py
+++ Working with Multidimensional numpy.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-import numpy as np
-
 array_2d = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 array_3d = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]], [[13, 14, 15], [16, 17, 18]]])
 
